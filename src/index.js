3321ed1b19151aa8f1337fc7875379760e89f9a5
Vinay072006 <vinaylogade683@gmail.com>
2025-06-25 16:43:44 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ dotenv.config({
 });
 
 // ✅ Call DB connection function
-connectDB();
+connectDB()
+
+.then(() =>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("error connection failed !!!",err)
+})
 
 // ✅ (Optional) Basic express setup here or inside connectDB
